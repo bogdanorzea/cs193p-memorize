4ad0d30f7f9640460adf988f85d366b945eb0c60
Bogdan Orzea <bogdan.orzea@gmail.com>
2020-05-28 13:25:21 -0400

    Completes Lesson 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
+        .padding()
+    }
+}
+
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+
+    var body: some View {
+        ZStack {
+            if (isFaceUp) {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("🃏")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
+        }
     }
 }
 
