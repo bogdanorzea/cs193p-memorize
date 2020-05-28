a4571fb040860c10a96df2f7714f0c641acc008c
Bogdan Orzea <bogdan.orzea@gmail.com>
2020-05-28 13:00:50 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Bogdan Orzea on 5/28/20.
+//  Copyright © 2020 Bogdan Orzea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
